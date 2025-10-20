302e9c42f0b3d55feadbfaa441cbd53623b1346f
IgorPribanic <igor.pribanic.1@gmail.com>
2025-10-20 20:21:24 +0200

    Ispis sume na kraju programa

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        int suma = 0 ;
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            suma += i;
         }
+        System.out.println("Suma brojeva je: " + suma);
     }
 }
